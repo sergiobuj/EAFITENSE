ca8bb6cb53d9b0292bd0fc3df4b8cbfd69a4f78c
Sergio Botero <sergiobuj@gmail.com>
2011-06-26 22:58:38 -0500

    added new rails app (stable version)

---
+++ eafitws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eafitws::Application.config.secret_token = 'dd94a5d5adcaf0de4ab52c52221a2c574c2219100abc3ec2fd95eb4d33a7efbdc20318ec50d6a1acf018c75da2dd610d1aeb4ec186eb3dc176fd0ed364cd2867'
